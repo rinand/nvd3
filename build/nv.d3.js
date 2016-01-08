faa6ef826a81628e21af18f4a9955b010678353b
rinand <tairinh@gmail.com>
2016-01-08 14:56:37 -0800

    update to production

--- build/nv.d3.js
+++ build/nv.d3.js
@@ -1,4 +1,4 @@
-/* nvd3 version 1.8.1-dev (https://github.com/novus/nvd3) 2015-12-29 */
+/* nvd3 version 1.8.1-dev (https://github.com/novus/nvd3) 2016-01-08 */
 (function(){
 
 // 
@@ -11288,6 +11288,7 @@ nv.models.scatter = function() {
         , useVoronoi   = true
         , duration     = 250
         , interactiveUpdateDelay = 300
+        , showLabel    = false //@zl
         ;
 
 
@@ -11420,9 +11421,10 @@ nv.models.scatter = function() {
                                      */
                                     var pX = getX(point,pointIndex);
                                     var pY = getY(point,pointIndex);
+                                    var gid = (typeof groupIndex === 'undefined')? 0 : groupIndex; //@zl
 
-                                    return [x(pX)+ Math.random() * 1e-4,
-                                            y(pY)+ Math.random() * 1e-4,
+                                    return [x(pX) + Math.floor(Math.random() * 100 + 1)/1000000, //@zl
+                                            y(pY) + gid/100 + Math.floor(Math.random() * 10+ 1)/1000000, //@zl -- this could cause issue in scatter func. 
                                         groupIndex,
                                         pointIndex, point]; //temp hack to add noise until I think of a better way so there are no duplicates
                                 })
@@ -11628,6 +11630,38 @@ nv.models.scatter = function() {
                 .style('stroke-opacity', 1)
                 .style('fill-opacity', .5);
 
+        if(showLabel)
+            {
+                    var titles =  groups.selectAll('text')
+                        .data(function(d) { return d.values }, pointKey);
+
+                    titles.enter().append('text')
+                        .style('fill', function (d,i) { return d.color })
+                        .style('stroke-opacity', 0)
+                        .style('fill-opacity', 1)
+                        .attr('x', function(d,i) { return nv.utils.NaNtoZero(x0(getX(d,i))) + Math.sqrt(z(getSize(d,i))/Math.PI) })
+                        .attr('y', function(d,i) { return nv.utils.NaNtoZero(y0(getY(d,i))) })
+                        .text(function(d,i){return d.tooltip;});
+
+                    titles.exit().remove();
+
+                    groups.exit().selectAll('text.nv-point').transition()
+                        .attr('x', function(d,i) { return nv.utils.NaNtoZero(x(getX(d,i))) })
+                        .attr('y', function(d,i) { return nv.utils.NaNtoZero(y(getY(d,i))) })
+                        .remove();
+
+                    titles.each(function(d,i) {
+                      d3.select(this)
+                        .classed('nv-point', true)
+                        .classed('nv-point-' + i, false)
+                        .classed('hover',false);
+
+                    });
+                     titles.transition()
+                         .attr('x', function(d,i) { return nv.utils.NaNtoZero(x(getX(d,i))) + Math.sqrt(z(getSize(d,i))/Math.PI) })
+                        .attr('y', function(d,i) { return nv.utils.NaNtoZero(y(getY(d,i))) });
+            }
+
             // create the points, maintaining their IDs from the original data set
             var points = groups.selectAll('path.nv-point')
                 .data(function(d) {
@@ -11785,7 +11819,8 @@ nv.models.scatter = function() {
             if (useVoronoi === false) {
                 clipVoronoi = false;
             }
-        }}
+        }},
+        showLabel: {get: function(){return showLabel;}, set: function(_){ showLabel = _;}},
     });
 
     nv.utils.initOptions(chart);
@@ -11826,6 +11861,7 @@ nv.models.scatterChart = function() {
         , dispatch = d3.dispatch('stateChange', 'changeState', 'renderEnd')
         , noData       = null
         , duration = 250
+        , showLabel    = false //@zl
         ;
 
     scatter.xScale(x).yScale(y);
@@ -11973,8 +12009,9 @@ nv.models.scatterChart = function() {
                 .color(data.map(function(d,i) {
                     d.color = d.color || color(d, i);
                     return d.color;
-                }).filter(function(d,i) { return !data[i].disabled }));
-
+                }).filter(function(d,i) { return !data[i].disabled }))
+                .showLabel(showLabel);
+            
             wrap.select('.nv-scatterWrap')
                 .datum(data.filter(function(d) { return !d.disabled }))
                 .call(scatter);
@@ -12000,23 +12037,24 @@ nv.models.scatterChart = function() {
                 .style('stroke-opacity', 0);
 
             // don't add lines unless we have slope and intercept to use
+            // check intercept only to support slope = 0 or infinite
             regLine.filter(function(d) {
-                return d.intercept && d.slope;
+                return d.intercept;
             })
                 .watchTransition(renderWatch, 'scatterPlusLineChart: regline')
-                .attr('x1', x.range()[0])
-                .attr('x2', x.range()[1])
+                .attr('x1', function(d, i) { return (typeof d.slope !== 'undefined')? x.range()[0] : x(d.intercept); })
+                .attr('x2', function(d, i) { return  (typeof d.slope !== 'undefined')? x.range()[1] : x(d.intercept); })
                 .attr('y1', function (d, i) {
-                    return y(x.domain()[0] * d.slope + d.intercept)
+                   return (typeof d.slope !== 'undefined')? y(x.domain()[0] * d.slope + d.intercept) : y.range()[0]; 
                 })
                 .attr('y2', function (d, i) {
-                    return y(x.domain()[1] * d.slope + d.intercept)
+                    return (typeof d.slope !== 'undefined')? y(x.domain()[1] * d.slope + d.intercept) : y.range()[1];
                 })
                 .style('stroke', function (d, i, j) {
                     return color(d, j)
                 })
                 .style('stroke-opacity', function (d, i) {
-                    return (d.disabled || typeof d.slope === 'undefined' || typeof d.intercept === 'undefined') ? 0 : 1
+                    return (d.disabled || (typeof d.slope === 'undefined' && typeof d.intercept === 'undefined')) ? 0 : 1
                 });
 
             // Setup Axes
@@ -12168,7 +12206,8 @@ nv.models.scatterChart = function() {
             legend.color(color);
             distX.color(color);
             distY.color(color);
-        }}
+        }},
+        showLabel: {get: function(){return showLabel;}, set: function(_){showLabel=_;}}
     });
 
     nv.utils.inheritOptions(chart, scatter);
